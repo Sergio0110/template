fd6ca948b6bf18f3d739435d2ae57784a1480273
Sergio0110 <jonsed@mail.ru>
2023-11-25 13:07:09 +0300

    пееделан footer

---
+++ css/phone.js
@@ -0,0 +1,5 @@
+document.querySelector('.header__contacts').addEventListener('click', function() {
+  // Получаем ссылку на номер телефона и симулируем клик по ней
+  var phoneLink = document.getElementById('phoneLink');
+  phoneLink.click();
+});
